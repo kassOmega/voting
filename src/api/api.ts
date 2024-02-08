1187726b1d3c054f4152afaa4b410b3a796d01d0
Kassahun Takele <kass3me@gmail.com>
2024-02-08 15:25:18 +0300

    url updated

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,13 +6,13 @@ export const useGetNominee = () =>
   useQuery<Response[]>({
     queryKey: ["nominee"],
     queryFn: async () =>
-      axios.get("http://localhost:3000/nominees").then((res) => res.data),
+      axios.get("http://localhost:3000/api/nominees").then((res) => res.data),
   });
 
 export const useCreateNominee = () =>
   useMutation({
     mutationFn: async (payload: Request) =>
       axios
-        .post("http://localhost:3000/nominees", payload)
+        .post("http://localhost:3000/api/nominees", payload)
         .then((res) => res.data),
   });
