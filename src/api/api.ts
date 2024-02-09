ca23e67696f6d625e6f7ec11953b5a683a17f10c
Kassahun Takele <kass3me@gmail.com>
2024-02-09 16:24:52 +0300

    api endpoint to update vote added

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { CreateRequest, Request, Response } from "../model/nominee";
+import { CreateRequest, Request, Response, UpdateVote } from "../model/nominee";
 
 export const useGetNominee = () =>
   useQuery<Response[]>({
@@ -14,5 +14,12 @@ export const useCreateNominee = () =>
     mutationFn: async (payload: { fullName: string }[]) =>
       axios
         .post("http://localhost:3000/api/nominees", payload)
+        .then((res) => res.data),
+  });
+export const useUpdateVoteNominee = () =>
+  useMutation({
+    mutationFn: async (payload: UpdateVote[]) =>
+      axios
+        .patch("http://localhost:3000/api/nominees/vote", payload)
         .then((res) => res.data),
   });
