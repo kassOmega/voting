e0e37b3cd02a649f27372e46e83ad8411bcf21bf
Kassahun Takele <kass3me@gmail.com>
2024-02-09 16:25:22 +0300

    nominee show-bar added

---
+++ src/components/nomineeItem.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { LinearProgress, Stack, Typography } from "@mui/material";
+import { Response } from "../model";
+
+export const NomineeItem = ({
+  nominee,
+  totalVotes,
+}: {
+  nominee: Response;
+  totalVotes: number;
+}) => {
+  const progress = Math.round((nominee.vote / totalVotes) * 100);
+  const color = totalVotes <= 9 ? "primary" : "secondary"; // Adjust color logic as needed
+
+  return (
+    <Stack width={"40%"}>
+      <Typography variant="h5" alignSelf={"center"}>
+        {nominee.fullName}
+      </Typography>
+      <LinearProgress variant="determinate" value={progress} color={color} />
+      <Typography variant="caption">{`${nominee.vote} votes`}</Typography>
+    </Stack>
+  );
+};
