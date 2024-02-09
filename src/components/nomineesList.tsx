d14c05d0183fa8ff4125d21dd7c4622b91676360
Kassahun Takele <kass3me@gmail.com>
2024-02-09 16:25:36 +0300

    updated

--- src/components/nomineesList.tsx
+++ src/components/nomineesList.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { useGetNominee } from "../api";
 import { Response } from "../model";
+import { List, ListItem, ListItemText } from "@mui/material";
+import { NomineeItem } from "./nomineeItem";
 
 export const NomineesList = ({ nominees }: { nominees: Response[] }) => {
+  const totalVotes = nominees.reduce((acc, nominee) => acc + nominee.vote, 0);
+  const sortedNominees = [...nominees].sort((a, b) => b.vote - a.vote);
   return (
-    <ul>
-      {nominees.map((nominee: any) => (
-        <li key={nominee.id}>
-          {nominee.fullName}
-          <input type="checkbox" value={nominee.id} />
-        </li>
+    <List>
+      {sortedNominees.map((nominee, index) => (
+        <ListItem key={nominee.id}>
+          <ListItemText
+            primary={<NomineeItem nominee={nominee} totalVotes={index} />}
+          />
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
