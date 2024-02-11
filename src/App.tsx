3045a4349d1b5736d8bede84a78fbe70a29d3e43
Kassahun Takele <kass3me@gmail.com>
2024-02-11 09:28:13 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
 import { Main } from "./main";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
 const queryClient = new QueryClient();
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <Main />
+        <ThemeProvider theme={theme}>
+          <Main />
+        </ThemeProvider>
       </QueryClientProvider>
-    </div>
+    </BrowserRouter>
   );
 }
 
