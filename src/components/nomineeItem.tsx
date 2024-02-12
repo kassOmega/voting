1c433a177b31c35186f2a1df0a2196e24c412635
Kassahun Takele <kass3me@gmail.com>
2024-02-12 11:07:09 +0300

    component added

--- src/components/nomineeItem.tsx
+++ src/components/nomineeItem.tsx
@@ -4,21 +4,35 @@ import { Response } from "../model";
 
 export const NomineeItem = ({
   nominee,
+  index,
   totalVotes,
 }: {
   nominee: Response;
+  index: number;
   totalVotes: number;
 }) => {
-  const progress = Math.round((nominee.vote / totalVotes) * 100);
-  const color = totalVotes <= 9 ? "primary" : "secondary";
+  const progress = nominee.vote
+    ? Math.round((nominee.vote / totalVotes) * 100)
+    : 0;
+  const color = index < 9 ? "success" : "error";
+  const typoColor = color === "success" ? "green" : "red";
 
   return (
-    <Stack width={"40%"}>
-      <Typography variant="h5" alignSelf={"center"}>
+    <Stack>
+      <Typography variant="h5" alignSelf={"center"} color={typoColor}>
         {nominee.fullName}
       </Typography>
       <LinearProgress variant="determinate" value={progress} color={color} />
-      <Typography variant="caption">{`${nominee.vote} votes`}</Typography>
+      <Stack direction="row" justifyContent="space-between">
+        <Typography
+          variant="caption"
+          color={typoColor}
+        >{`${nominee.vote} votes`}</Typography>
+        <Typography
+          variant="caption"
+          color={typoColor}
+        >{`${progress} %`}</Typography>
+      </Stack>
     </Stack>
   );
 };
