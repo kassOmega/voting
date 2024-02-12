9f545a47e75b1ccc06083afd2a6e804e1f857ec8
Kassahun Takele <kass3me@gmail.com>
2024-02-12 11:09:16 +0300

    nominee list made awesome

--- src/components/nomineesList.tsx
+++ src/components/nomineesList.tsx
@@ -1,21 +1,54 @@
 import React from "react";
-import { useGetNominee } from "../api";
 import { Response } from "../model";
-import { List, ListItem, ListItemText } from "@mui/material";
+import {
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { NomineeItem } from "./nomineeItem";
 
 export const NomineesList = ({ nominees }: { nominees: Response[] }) => {
   const totalVotes = nominees.reduce((acc, nominee) => acc + nominee.vote, 0);
   const sortedNominees = [...nominees].sort((a, b) => b.vote - a.vote);
   return (
-    <List>
-      {sortedNominees.map((nominee, index) => (
-        <ListItem key={nominee.id}>
-          <ListItemText
-            primary={<NomineeItem nominee={nominee} totalVotes={index} />}
-          />
-        </ListItem>
-      ))}
-    </List>
+    <Stack
+      padding={4}
+      spacing={4}
+      flex={1}
+      justifySelf="center"
+      alignSelf={"center"}
+    >
+      <Stack spacing={8} direction="row">
+        <Typography variant="h2" color="#2279f1">
+          Live Results
+        </Typography>
+      </Stack>
+
+      <Typography variant="h4" alignSelf="center">
+        Total counted votes {totalVotes}
+      </Typography>
+      <List>
+        <Grid container spacing={2}>
+          {sortedNominees.map((nominee, index) => (
+            <Grid item xs={4} key={nominee.id}>
+              <ListItem>
+                <ListItemText
+                  primary={
+                    <NomineeItem
+                      nominee={nominee}
+                      index={index}
+                      totalVotes={totalVotes}
+                    />
+                  }
+                />
+              </ListItem>
+            </Grid>
+          ))}
+        </Grid>
+      </List>
+    </Stack>
   );
 };
