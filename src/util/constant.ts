2fc999d4798e8750162ffcd205c7ac910059b092
Kassahun Takele <kass3me@gmail.com>
2024-02-14 13:43:26 +0300

    backend URL added

--- src/util/constant.ts
+++ src/util/constant.ts
@@ -2,4 +2,5 @@ export const Constant = {
   URL:
     "http://192.168.9.115:3000/api/nominees" ||
     "http://localhost:3000/api/nominees",
+  ASIURL: "https://asi.dagu.app/api/admin/promised-share",
 };
